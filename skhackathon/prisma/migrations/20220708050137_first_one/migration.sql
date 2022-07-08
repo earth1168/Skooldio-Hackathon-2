64f1d515a12ba76603f3f73bde665e1a6bc54375
Earth <pimanus1234@gmail.com>
2022-07-08 13:32:56 +0700

    Deploy postgres on Heroku

--- skhackathon/prisma/migrations/20220708050137_first_one/migration.sql
+++ skhackathon/prisma/migrations/20220708050137_first_one/migration.sql
@@ -10,11 +10,19 @@ CREATE TABLE "Keyword" (
 -- CreateTable
 CREATE TABLE "Course" (
     "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "name" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "instructor" TEXT NOT NULL,
     "imageUrl" TEXT NOT NULL,
 
     CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "question" TEXT NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
 );
